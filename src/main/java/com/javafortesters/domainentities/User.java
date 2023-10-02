a27c2c16c4a7dfa758a3ecda3aefa5dacafaddc0
Marcin <stev1989@gmail.com>
2023-10-02 21:38:54 +0200

    commit User.java /chapter 6 - Constructors, Fields, Getter & Setter/

---
+++ src/main/java/com/javafortesters/domainentities/User.java
@@ -0,0 +1,34 @@
+package com.javafortesters.domainentities;
+
+public class User {
+
+    private String username;
+    private String password;
+
+    public User () {
+        this ("username", "password");
+        /* above better of the same as below:
+        username = "username";
+        password = "password";
+         */
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
