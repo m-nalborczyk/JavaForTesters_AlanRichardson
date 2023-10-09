d891ac5cd6c8627420cb383dd9408a17085ebc17
Marcin <stev1989@gmail.com>
2023-10-09 22:50:45 +0200

    commit ArraysIterationTests.java - next part /chapter 9 - Arrays and For Loops Iterations/

--- src/test/java/com/javafortesters/chap009arraysiteration/examples/ArraysIterationTests.java
+++ src/test/java/com/javafortesters/chap009arraysiteration/examples/ArraysIterationTests.java
@@ -2,6 +2,8 @@ package com.javafortesters.chap009arraysiteration.examples;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
 
 public class ArraysIterationTests {
@@ -49,6 +51,60 @@ public class ArraysIterationTests {
             days = days + "|" + i + "-" + workdays[i];
         }
         assertEquals("|0-Monday|1-Tuesday|2-Wednesday|3-Thursday|4-Friday", days);
+    }
+
+    @Test
+    public void testArraysMethods() {
+        String[] weekDays;
+        weekDays = Arrays.copyOf(workdays, 7);
+        weekDays[5] = "Saturday";
+        weekDays[6] = "Sunday";
+        for (String weekday : weekDays) {
+            System.out.println(weekday);
+        }
+
+        String[] truncatedWeekDays = Arrays.copyOf(weekDays, 3);
+        for (String weekday : truncatedWeekDays) {
+            System.out.println(weekday);
+        }
+        assertEquals("Tuesday", truncatedWeekDays[1]);
+
+        String[] subsetDays = Arrays.copyOfRange(weekDays, 2, 5);
+        for (String weekday : subsetDays) {
+            System.out.println(weekday);
+        }
+
+        int[] minusOneFullArray = new int[30];
+        Arrays.fill(minusOneFullArray, -1);
+        int index = 0;
+        for (int number : minusOneFullArray) {
+            index = 1 + index;
+            System.out.println(index + ": " + number);
+        }
+
+        int[] tenItems = {0, 0, 0, 0, 0, 1, 1, 1, 1, 1};
+        //fill cells 5-9 with '2'
+        Arrays.fill(tenItems, 5, 10, 2);
+        //0-4 are untouched
+        assertEquals(0, tenItems[0]);
+        assertEquals(0, tenItems[4]);
+        //5-9 now equal 2
+        assertEquals(2, tenItems[5]);
+        assertEquals(2, tenItems[9]);
+        for (int number : tenItems) {
+            System.out.println(number);
+        }
+    }
+
+        @Test
+        public void testSorting(){
+        int[] outOfOrder = {2,4,2,1,5,0};
+        int[] sortedArray = Arrays.copyOf(outOfOrder, outOfOrder.length);
+        Arrays.sort(sortedArray);
+        System.out.println("Original array: " + Arrays.toString(outOfOrder));
+        System.out.println("Sorted array: " + Arrays.toString(sortedArray));
+        assertEquals(5, sortedArray[sortedArray.length-1]);
+
     }
 
 }
