d89d919a6a112f17df93be4fcd47a8fb5c19cac8
Marcin <stev1989@gmail.com>
2023-11-16 21:33:09 +0100

    commit ExceptionsTest.java - next part /chapter 11 - Introducing Exceptions/

--- src/test/java/com/javafortesters/chap011exceptions/examples/ExceptionsTest.java
+++ src/test/java/com/javafortesters/chap011exceptions/examples/ExceptionsTest.java
@@ -9,12 +9,53 @@ public class ExceptionsTest {
     @Test
     public void throwANullPointerException(){
         Integer age = null;
-
         String ageAsString = age.toString();
+        String yourAge = "You are " + ageAsString + " years old.";
+        assertEquals("You are 18 years old.", yourAge);
+    }
+
+    @Test
+    public void catchANullPointerException(){
+        Integer age = null;
+        String ageAsString;
+
+        try {
+            ageAsString = age.toString();
+        } catch (NullPointerException e){
+
+            //as exception in variable is object it has methods such below that can be used
+            System.out.println("getMessage - " + e.getMessage());
+            System.out.println("getStacktrace - " + e.getStackTrace());
+            System.out.println("printStacktrace - ");
+            e.printStackTrace();
+
+            age = 18;
+            ageAsString = age.toString();
+        }
 
         String yourAge = "You are " + ageAsString + " years old.";
+        assertEquals("You are 18 years old.", yourAge);
+    }
 
+    @Test
+    public void catchMoreThanOneException(){
+        Integer age = null;
+        String ageAsString;
+
+        try {
+            ageAsString = age.toString();
+        } catch (NullPointerException e){
+            age = 18;
+            ageAsString = age.toString();
+        } catch (IllegalArgumentException e){ //as we handle 2 possible exceptions we need to make sure ageAsString is initialized here as well
+            System.out.println("Illegal Argument - " + e.getMessage());
+            age = 18;
+            ageAsString = age.toString();
+
+        }
+        String yourAge = "You are " + ageAsString + " years old.";
         assertEquals("You are 18 years old.", yourAge);
     }
 
 }
+
